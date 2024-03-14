0fb84e8b6bdf27cfde10af12182925aea744edef
TheHGuito1602 <hguito1602@gmail.com>
2024-03-13 18:28:48 -0600

    Logout completado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,53 @@
-import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa' 
-import {Link} from 'react-router-dom'
+import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout, reset } from '../features/auth/authSlice'
 
 const Header = () => {
-  return (
-    <header className='header'>
-        <div className="logo">
-            <Link to='/'>App de Tareas</Link>
-        </div>
-        <ul>
-            <li>
-                <Link to='/login'>
-                    <FaSignInAlt></FaSignInAlt>
-                    Login
-                </Link>
-            </li>
-            <li>
-                <Link to='register'>
-                    <FaUser></FaUser>
-                    Registrar
-                </Link>
-            </li>
-        </ul>
-    </header>
-  )
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const { user } = useSelector((state) => state.auth)
+
+    const onLogout = () =>{
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/login')
+    }
+
+    return (
+        <header className='header'>
+            <div className="logo">
+                <Link to='/'>App de Tareas</Link>
+            </div>
+            <ul>
+                {user ? (
+                    <li>
+                        <button className='btn' onClick={onLogout}>
+                            <FaSignInAlt></FaSignInAlt>
+                            Logout
+                        </button>
+                    </li>
+                ) : (
+                    <>
+                        <li>
+                            <Link to='/login'>
+                                <FaSignInAlt></FaSignInAlt>
+                                Login
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='register'>
+                                <FaUser></FaUser>
+                                Registrar
+                            </Link>
+                        </li>
+                    </>
+                )}
+            </ul>
+        </header>
+    )
 }
 
 export default Header
